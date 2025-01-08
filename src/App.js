0f22c0506d64486ce2c547915621cdaa9b3b43ea
KronosFPV <137655816+KronosFPV@users.noreply.github.com>
2025-01-08 10:55:09 +0100

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>Drone Calculator</h1>
+      <p>Coming soon: A tool to calculate the profitability of building drones!</p>
+    </div>
+  );
+}
+
+export default App;
